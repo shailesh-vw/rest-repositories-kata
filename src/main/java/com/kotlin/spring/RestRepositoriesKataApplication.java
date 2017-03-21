46424420acaa96f6f800bd117b7839d0eac8c0de
Shailesh <shailesh.waman@gmail.com>
2017-03-21 22:37:06 +0100

    simple kata to learn spring boot rest repository

---
+++ src/main/java/com/kotlin/spring/RestRepositoriesKataApplication.java
@@ -0,0 +1,12 @@
+package com.kotlin.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestRepositoriesKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestRepositoriesKataApplication.class, args);
+	}
+}
